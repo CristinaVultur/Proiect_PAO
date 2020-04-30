bb9f03dcdab90130e616f550ec46925774e6bc4c
CristinaVultur <48892856+CristinaVultur@users.noreply.github.com>
2020-04-30 21:48:47 +0300

    Update Concert.java

--- src/com/proiect/PAO/Concert.java
+++ src/com/proiect/PAO/Concert.java
@@ -6,12 +6,12 @@ public class Concert extends Eveniment {
     private String numeArtist, tip;//live, acustic, solo, orchestra, cor, ansamblu, trupa
 
     public Concert(String nume, int pretBilet, String gen, int durata, String data,
-                   Locuri locuri, String numeArtist, String tip) throws ParseException {
+                   Locuri locuri, String numeArtist) throws ParseException {
         super(nume, pretBilet, gen, durata, data, locuri);
         this.numeArtist = numeArtist;
         this.locuri = locuri;
     }
-    public Concert() throws ParseException {
+    public Concert(String value, int i, String s, int parseInt) throws ParseException {
         super();
     }
 
@@ -21,13 +21,5 @@ public class Concert extends Eveniment {
 
     public void setNumeArtist(String numeArtist) {
         this.numeArtist = numeArtist;
-    }
-
-    public String getTip() {
-        return tip;
-    }
-
-    public void setTip(String tip) {
-        this.tip = tip;
     }
 }
