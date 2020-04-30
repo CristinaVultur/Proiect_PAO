7b8e0a066f3e9b4814cb6df04dbc82f0f5f5b04e
CristinaVultur <48892856+CristinaVultur@users.noreply.github.com>
2020-04-30 21:51:59 +0300

    Update Main.java

--- src/com/proiect/PAO/Main.java
+++ src/com/proiect/PAO/Main.java
@@ -1,75 +1,104 @@
 package com.proiect.PAO;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 
 public class Main {
 
 
-    public static void main(String[] args) throws ParseException {
-	Client c = new Client("Gigel",29,"gigel.ionescu@yahoo.com");
-	ArrayList<Eveniment> Events = new ArrayList<>();
+    public static void main(String[] args) throws ParseException, IOException {
 
-    Locuri Sala = new Locuri(new int[]{50, 50, 30, 30, 15, 15, 5, 5});
-    Sala.creareLocuri();
+        Client c = new Client("Gigel",29,"gigel.ionescu@yahoo.com");
+        ArrayList<Eveniment> events = new ArrayList<>();
+        ArrayList<Client> clienti = SingletonFiles.getInstance().readClienti("clienti.csv");
 
-    Locuri Sala1 = new Locuri(new int[]{60, 60, 30, 30, 20, 20, 10, 10});
-    Sala1.creareLocuri();
+        SingletonFiles.getInstance().writeClient("clienti.csv",c);
 
-    Teatru teatru = new Teatru("Nume de Teatru",100,"drama",2,"Feb 28, 2020",Sala,"nume trupa","nume regizor");
-    Events.add(teatru);
+        events.addAll(SingletonFiles.getInstance().citireTeatru("teatru.csv"));
 
-    Opera opera = new Opera("Nume Opera", 200, "tragic",3,"Mar 13, 2020", Sala1,"Trupa Opera","Orchestra", "Dirijor" );
-    Events.add(opera);
+        events.addAll(SingletonFiles.getInstance().citireConcert("concert.csv"));
 
-    Locuri Sala2 = new Locuri(new int[]{40, 40, 30, 30, 10, 10, 0, 0});
-    Sala2.creareLocuri();
+        events.addAll(SingletonFiles.getInstance().citireOpera("opera.csv"));
 
-    Film film = new Film("Nume Film", 20,"comedie", 1,"Apr 12, 2020",Sala2,2,"romana",false);
-    Events.add(film);
+        events.addAll(SingletonFiles.getInstance().citireFilm("film.csv"));
 
-    Locuri Sala3 = new Locuri(new int[]{50, 50, 30, 30, 15, 15, 5, 5});
-    Concert concert = new Concert("Nume Concert", 50, "rock", 4,"Apr 20, 2021",Sala3,"Artist","live");
-
-    Events.add(concert);
-    //caut evenimentul dupa nume
-
-    Eveniment e = ClientService.cautaEvenimentDupaNume(Events,"Nume de Teatru");
-
-    // afisare locuri disponibile la un anumit spectacol
-    ClientService.afisareLocuriDisponibile(e);
-
-    //rezerva bilet
-    ClientService.rezervareBilet(e, 26,"normal",c);
-
-    //afisare locuri rezervate de cliente
-    ClientService.afisareLocuriRezervate(c);
-
-    //anuleaza rezervarea la un anumit spectacol
-    ClientService.anuleazaRezervarea(e,c);
-
-    ClientService.afisareLocuriRezervate(c);
-
-    //afiseaza evenimentele la care mai sunt locuri disponibile
-    ClientService.AfiseazaToateEvenimenteleDisponibile(Events);
-
-    //caut evenimente dupa gen
-    ClientService.cautareEvenimenteDupaGen(Events,"comedie");
-
-    //caut locurile de un anumit tip disponibile la evenimentul e
-    ClientService.cautareLocuri(e,"VIP");
-
-    //caut doar evenimentele de un anumit tip: opera, film etc
-
-    ClientService.cautaDoarOSublcasa(Events, "Opera");
-
-    //caut evenient dupa data
-    ClientService.cautareEvenimenteInData(Events,"Feb 30, 2020");
+        Locuri Sala = new Locuri(new int[]{50, 50, 30, 30, 15, 15, 5, 5});
+        Sala.creareLocuri();
+        Teatru teatru = new Teatru("Nume de Teatru", 100, "drama", 2, "2020-02-24", Sala, "nume trupa", "nume regizor");
 
 
-    ClientService.rezervareBilet(e,26,"normal",c);
+        SingletonFiles.getInstance().writeTeatru("teatru.csv",teatru);
 
-    //schimb un bilet rezervat
-    ClientService.schimbaLocul(e,46,"VIP",c);
+        events.add(teatru);
+
+        Locuri Sala1 = new Locuri(new int[]{60, 60, 30, 30, 20, 20, 10, 10});
+        Sala1.creareLocuri();
+
+        Opera opera = new Opera("Nume Opera", 200, "tragic", 3, "2020-03-02", Sala1, "Trupa Opera", "Orchestra", "Dirijor");
+        events.add(opera);
+
+        SingletonFiles.getInstance().writeOpera("opera.csv",opera);
+
+        Locuri Sala2 = new Locuri(new int[]{40, 40, 30, 30, 10, 10, 0, 0});
+        Sala2.creareLocuri();
+
+        Film film = new Film("Nume Film", 20, "comedie", 1, "2020-04-23", Sala2, 2, "romana", false);
+
+       SingletonFiles.getInstance().writeFilm("film.csv",film);
+        events.add(film);
+
+        Locuri Sala3 = new Locuri(new int[]{50, 50, 30, 30, 15, 15, 5, 5});
+        Concert concert = new Concert("Nume Concert", 50, "rock", 4, "2021-03-12", Sala3, "Artist");
+
+        SingletonFiles.getInstance().writeConcert("concert.csv",concert);
+
+        events.add(concert);
+        //caut evenimentul dupa nume
+        try {
+            Eveniment e = ClientService.cautaEvenimentDupaNume(events, "Nume de Teatru");
+            Eveniment e2 = ClientService.cautaEvenimentDupaNume(events, "Nume Film1");
+
+            // afisare locuri disponibile la un anumit spectacol
+            ClientService.afisareLocuriDisponibile(e);
+
+            //rezerva bilet
+            ClientService.rezervareBilet(e2, 27, "normal", clienti.get(0));
+            ClientService.rezervareBilet(e, 26, "normal", clienti.get(1));
+
+
+            //afisare locuri rezervate de cliente
+            ClientService.afisareLocuriRezervate(clienti.get(1));
+
+            //anuleaza rezervarea la un anumit spectacol
+            ClientService.anuleazaRezervarea(e,  clienti.get(1));
+
+            ClientService.afisareLocuriRezervate( clienti.get(0));
+
+            //afiseaza evenimentele la care mai sunt locuri disponibile
+            ClientService.AfiseazaToateEvenimenteleDisponibile(events);
+
+            //caut evenimente dupa gen
+            ClientService.cautareEvenimenteDupaGen(events, "comedie");
+
+            //caut locurile de un anumit tip disponibile la evenimentul e
+            ClientService.cautareLocuri(e, "VIP");
+
+            //caut doar evenimentele de un anumit tip: opera, film etc
+
+            ClientService.cautaDoarOSublcasa(events, "Opera");
+
+            //caut evenient dupa data
+            ClientService.cautareEvenimenteInData(events, "Feb 30, 2020");
+
+
+            ClientService.rezervareBilet(e, 26, "normal",  clienti.get(2));
+
+            //schimb un bilet rezervat
+            ClientService.schimbaLocul(e, 46, "VIP",  clienti.get(2));
+        } catch (Exception e) {
+            System.out.println("exception: " + e.getMessage());
+
+        }
     }
 }
